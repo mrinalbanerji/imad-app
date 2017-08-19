63911c59fe0ae61597da0ed132ba9d7ce6ec45e4
mrinalbanerji <30766359+mrinalbanerji@users.noreply.github.com>
2017-08-19 11:59:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = "New Value";
 
 //Move the image
 var image = document.getElementById("madi");
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    image.style.marginLeft = marginLeft + 'px';
+}
 image.onclick = function(){
-    image.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
 }
