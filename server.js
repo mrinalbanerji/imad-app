5ca7830329d450800821c7d7f3976f1be0487550
mrinalbanerji <30766359+mrinalbanerji@users.noreply.github.com>
2017-08-24 10:08:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/server-name/:name', function (req, res){
+app.get('/submit-name/:name', function (req, res){
     //get the name from the request object
     var name = req.params.name;
     
